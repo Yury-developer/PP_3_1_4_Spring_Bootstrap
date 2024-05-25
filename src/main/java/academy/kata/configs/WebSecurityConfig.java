772f5f602cae28b0b666dc78359b11a00a101751
Yury_Home_PC <yury.connect@gmail.com>
2024-05-25 16:07:18 +0300

    сейчас буду менять связи user и роль 1:n на n:n

--- src/main/java/academy/kata/configs/WebSecurityConfig.java
+++ src/main/java/academy/kata/configs/WebSecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 
 @Configuration // можно не указывать, т.к. по цепочке достанется
@@ -85,7 +88,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN")
                 .build();
 
-
+// Так было  в базовом варианте, пароли хранились в незашифрованном виде
 //        UserDetails user =
 //                User.withDefaultPasswordEncoder()
 //                        .username("user")
@@ -102,4 +105,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         return new InMemoryUserDetailsManager(user, admin);
     }
+
+
+
+//    // аутентификация jdbcAuthenticator
+//    @Bean
+//    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) { // при старте будем засовывать в базу этих пользователей.
+//        UserDetails user = User.builder() // минимальная информация о пользователе
+//                .username("user")
+////                .password("{bcrypt}$2a$10$4sqSaa4di9Y511hsKxoe3u9Ri85EQOxqbwfFV0RAAAnxUMKYll542") // password=user
+//                .password("{bcrypt}$2a$10$Iom7deSLgxAxykvuANH2s.KpMy5xWbjgQmcsuiycdJt0UMoQflKaC") // password=u
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder() // минимальная информация о пользователе
+//                .username("admin")
+////                .password("{bcrypt}$2a$10$yRrs63ZIWDxNIB6LmmwVu.h243nBteNei2Gs4Llt7bymuJZQOoRYG") // password=admin
+//                .password("{bcrypt}$2a$10$dWJyopJqWj/PDxEozd6MzOzTwV.5c2GNoU6hUiou0YOF2CHkfDoZK") // password=a
+//                .roles("ADMIN")
+//                .build();
+//        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+//
+//        if (manager.userExists(user.getUsername())) {
+//            manager.deleteUser(user.getUsername());
+//        }
+//        if (manager.userExists(admin.getUsername())) {
+//            manager.deleteUser(admin.getUsername());
+//        }
+//
+//        manager.createUser(user);
+//        manager.createUser(admin);
+//
+//        return manager;
+//    }
+
 }
