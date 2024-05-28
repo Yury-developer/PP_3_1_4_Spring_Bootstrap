ceb4a71da1e4b8ba0c0c819b7c4790045d3d6420
Yury_Home_PC <yury.connect@gmail.com>
2024-05-28 08:10:13 +0300

    save

--- src/main/java/academy/kata/configs/MvcConfig.java
+++ src/main/java/academy/kata/configs/MvcConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    // при обращении 'http://localhost:8080/user' перебросит сразу на 'user.html' минуя контроллер
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/user").setViewName("user");
+        registry.addViewController("/user").setViewName("user"); // переделать!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 }
